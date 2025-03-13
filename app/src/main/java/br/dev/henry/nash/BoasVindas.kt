c1514ea3db180a39be808e6018e8ac16e3f42fbe
realsethy <henryzarlenga12@gmail.com>
2025-03-12 21:18:43 -0300

    ui criadas

---
+++ app/src/main/java/br/dev/henry/nash/BoasVindas.kt
@@ -0,0 +1,20 @@
+package br.dev.henry.nash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BoasVindas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_boasvindas)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
